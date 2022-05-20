8c72c9172bda6e8bdbeb8a2d2c16e386ffcf5ffd
swatidalal <swati15091999@gmail.com>
2022-05-20 12:37:11 +0530

    commit

---
+++ BillingAndInventoryManagementSystem/FileHandlingClass.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingAndInventoryManagementSystem
+{
+    public class FileHandlingClass
+    {
+
+        public void User_Module()
+        {
+            FileStream fst = new FileStream(@"C:\Users\LENOVO\Desktop\Training\Users.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fst);
+            Console.Write("Enter the UserId:- ");
+            sw.Write("user id:- ");
+            sw.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the Firstname: ");
+            sw.Write("first name:- ");
+            sw.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the Lastname: ");
+            sw.Write("Last name:- ");
+            sw.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the Email: ");
+            sw.Write("Email:- ");
+            sw.WriteLine(Console.ReadLine());
+     
+
+            Console.Write("Enter the Phone: ");
+            sw.Write("phone no :-");
+            sw.WriteLine(Console.ReadLine());
+           
+            sw.Close();
+            fst.Close();
+        }
+        public void Category_Module()
+        {
+            FileStream fobj = new FileStream(@"C:\Users\LENOVO\Desktop\Training\Category.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sobj = new StreamWriter(fobj);
+            Console.Write("Enter the CategoryId: ");
+            sobj.Write("CAtegory id :- ");
+            sobj.WriteLine(Console.ReadLine());
+           
+
+            Console.Write("Enter the CategoryType: ");
+            sobj.Write("Categorytype:- ");
+            sobj.WriteLine(Console.ReadLine());
+
+            sobj.Close();
+            fobj.Close();
+        }
+        public void Product_Module()
+        {
+
+            FileStream fobj = new FileStream(@"C:\Users\LENOVO\Desktop\Training\Product.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sobj = new StreamWriter(fobj);
+            Console.Write("Enter the Productid: ");
+            sobj.Write("Product id:- ");
+            sobj.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the categorytype: ");
+            sobj.Write("Categorytype:-");
+            sobj.WriteLine(Console.ReadLine());
+           
+
+            Console.Write("Enter the productname: ");
+            sobj.Write("Product name");
+            sobj.WriteLine(Console.ReadLine());
+          
+
+            Console.Write("Enter the quantity: ");
+            sobj.Write("quantity:- ");
+            sobj.WriteLine(Console.ReadLine());
+           
+
+            Console.Write("Enter the price: ");
+            sobj.Write("price:-");
+            sobj.WriteLine(Console.ReadLine());
+          
+            sobj.Close();
+            fobj.Close();
+
+        }
+        public void Purchase_and_Sales_Module()
+        {
+
+
+            FileStream fobj = new FileStream(@"C:\Users\LENOVO\Desktop\Training\Purchase&Sale.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sobj = new StreamWriter(fobj);
+            Console.Write("Enter the SalesId: ");
+            sobj.Write("SalesId:- ");
+            sobj.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the Productid: ");
+            sobj.Write("Productid:-");
+            sobj.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the price: ");
+            sobj.Write("Price");
+            sobj.WriteLine(Console.ReadLine());
+            
+
+            Console.Write("Enter the salesdate: ");
+            sobj.Write("Salesdate");
+            sobj.WriteLine(Console.ReadLine());
+
+
+
+            sobj.Close();
+            fobj.Close();
+
+
+
+        }
+    }
+}
---
+++ BillingAndInventoryManagementSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingAndInventoryManagementSystem
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            FileHandlingClass fileobj = new FileHandlingClass();
+            Console.WriteLine("Enter the User Details :-");
+            fileobj.User_Module();
+            Console.WriteLine("\n");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the Category Details :-");
+            fileobj.Category_Module();
+            Console.WriteLine("\n");
+            Console.WriteLine();
+            Console.WriteLine("Enter the Product Details :-");
+            fileobj.Product_Module();
+            Console.WriteLine("\n");
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the PURCHASE AND Sales Details :-");
+            fileobj.Purchase_and_Sales_Module();
+            Console.WriteLine("\nEnter any key to continue");
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
