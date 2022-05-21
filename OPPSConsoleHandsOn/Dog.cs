d6b84683483e10be216a65079449582c5f30cd31
swatidalal <swati15091999@gmail.com>
2022-05-21 11:05:27 +0530

    file commites

---
+++ OPPSConsoleHandsOn/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPSConsoleHandson
+{
+    class Dog
+    {
+        public string breed;
+
+        public void bark()
+        {
+            Console.WriteLine("Bark Bark !!");
+
+        }
+    }
+}
+
