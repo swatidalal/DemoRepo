214d9ecc5c88693f7010ff590ea0467ff3f39648
swatidalal <swati15091999@gmail.com>
2022-06-02 21:29:23 +0530

    Initial Commit

---
+++ AsyncAndAwaitDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Task1();
+                Task2();
+                Console.ReadKey();
+        }
+
+            public static async Task Task1()
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(" Task1 1");
+                        // Do something
+                        Task.Delay(100).Wait();
+                    }
+                });
+            }
+
+
+            public static void Task2()
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    Console.WriteLine(" Task2 2");
+                    // Do something
+                    Task.Delay(10).Wait();
+                }
+            }
+    }
+}
+
