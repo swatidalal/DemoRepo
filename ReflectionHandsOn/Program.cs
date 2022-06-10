b63ce837f9606be88524adaeb5b60e273ac6c338
swatidalal <swati15091999@gmail.com>
2022-06-10 17:58:15 +0530

    Commit

---
+++ ReflectionHandsOn/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionHandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> list = new List<Student>();
+
+
+            Student obj = new Student { RollNo = 1, Name = "Swati" };
+            list.Add(obj);
+
+            obj = new Student { RollNo = 2, Name = "Shailja" };
+            list.Add(obj);
+           
+
+            Type t = typeof(Student);
+
+
+            //Use Reflection to find about
+            //any sort of data related to t
+            Console.WriteLine("Name : {0}", t.Name);
+            Console.WriteLine("Full Name : {0}", t.FullName);
+            Console.WriteLine("Namespace : {0}", t.Namespace);
+            Console.WriteLine("Base Type : {0}", t.BaseType);
+
+
+            Console.WriteLine("*****************************************************************************************");
+
+            Assembly executing = Assembly.GetExecutingAssembly();
+
+            // Array to store types of the assembly
+            Type[] types = executing.GetTypes();
+            foreach (var item in types)
+            {
+                // Display each type
+                Console.WriteLine("Class : {0}", item.Name);
+
+                // Array to store methods
+                MethodInfo[] methods = item.GetMethods();
+                foreach (var method in methods)
+                {
+                    // Display each method
+                    Console.WriteLine(" Method : {0}", method.Name);
+
+                    // Array to store parameters
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (var arg in parameters)
+                    {
+                        // Display each parameter
+                        Console.WriteLine("Parameter : {0} Type : {1}",
+                                                arg.Name, arg.ParameterType);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReflectionHandsOn/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionHandsOn
+{
+    internal class Student
+    {
+        public int RollNo{get;set;}
+
+        public string Name { get; set; }
+
+        // No Argument Constructor
+        public Student()
+        {
+            RollNo = 0;
+            Name = string.Empty;
+        }
+
+        // Parameterised Constructor
+        public Student(int rno, string n)
+        {
+            RollNo = rno;
+            Name = n;
+        }
+
+        // Method to Display Student Data
+        public void displayData()
+        {
+            Console.WriteLine("Roll Number : {0}", RollNo);
+            Console.WriteLine("Name : {0}", Name);
+        }
+        
+    }
+
+}
+
