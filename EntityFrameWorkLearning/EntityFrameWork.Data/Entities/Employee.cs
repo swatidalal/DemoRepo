986e1907a886373ff3fd64430ba2b9f48701fe40
swatidalal <swati15091999@gmail.com>
2022-06-20 21:59:48 +0530

    Ef core codeFirst HandsOn

---
+++ EntityFrameWorkLearning/EntityFrameWork.Data/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWork.Data.Entities
+{
+    public class Employee
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Key]
+
+        public int ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+
+
+        public ICollection<EmployeeEducation>? EducationList { get; set; }
+
+
+
+    }
+}
---
+++ EntityFrameWorkLearning/EntityFrameWork.Data/Migrations/20220620085943_InitialMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameWork.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeEducations",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UniversityName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PassingYear = table.Column<int>(type: "int", nullable: false),
+                    MarksPercentage = table.Column<int>(type: "int", nullable: false),
+                    EmployeeID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeEducations", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_EmployeeEducations_Employees_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeEducations_EmployeeID",
+                table: "EmployeeEducations",
+                column: "EmployeeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeEducations");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
